70dd0034cbe34b0b34f8deb2771b8ea78933530a
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2025-05-19 17:06:51 +0300

    refactoring and formating

--- learn-with-code/data-handling/recipe-viewer.py
+++ learn-with-code/data-handling/recipe-viewer.py
@@ -11,34 +11,27 @@ def load_file():
                     ingredients=lines[1].replace("ingredients:","").strip()
                     instructions = lines[2].replace("instructions:","").strip()
                     recipe_dict[name]={"ingredients":ingredients, "instructions":instructions}
-
-        return recipe_dict
-    
+        return recipe_dict    
     except:
         print("File is not found!")
-        return {}
-    
+        return {}   
 
-print("----------------START--------------------")
-reciepes=load_file()
+def main():
+    print("\n----------------START--------------------")
+    reciepes=load_file()
+    for name in reciepes:
+        print(f"\n----Recipe {name} Details------")  
+        print(f"Ingredients")
+        current_recipe_ingredients=reciepes[name]['ingredients'].strip().split(',')
+        current_recipe_instructions=reciepes[name]['instructions'].strip().split(',')
+        for ingredient  in current_recipe_ingredients:
+            print(f"- {ingredient.strip()}")            
+        print(f"Instructions")    
+        for instruction in current_recipe_instructions:
+            print(f"- {instruction.strip()}")
+    print ("\n-----------------END--------------------\n")
 
-for name in reciepes:
-    print(f"\n----Recipe {name} Details------")  
-    print(f"\nIngredients")
-    current_recipe_ingredients=reciepes[name]['ingredients'].strip().split(',')
-    current_recipe_instructions=reciepes[name]['instructions'].strip().split(',')
-
-    for ingredient  in current_recipe_ingredients:
-        print(f"- {ingredient.strip()}")
-        
-    print(f"\nInstructions")    
-    for instruction in current_recipe_instructions:
-        print(f"- {instruction.strip()}")
-
-
-print ("-----------------END--------------------")
-
-
+main()
     
 
 
