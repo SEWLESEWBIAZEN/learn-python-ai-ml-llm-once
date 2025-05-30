3bb13d7ac3a49992da092c4bd6fd181937a59631
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2025-05-30 08:56:08 +0300

    update fabonacci list generator

--- learn-with-code/advanced-python/generators-and-iterators.py
+++ learn-with-code/advanced-python/generators-and-iterators.py
@@ -1,12 +1,11 @@
 def fabonacci(max):
-    a,b,counter=0,1,0
-    while True:
-        if(counter>max):
-            return
+    a, b, counter = 0,1,0
+    while (counter<=max):
         yield a
-        a,b =b, a+b        
+        a,b = b,a+b
         counter+=1
+    
 
 f=fabonacci(10)
 for x in f:
-    print(x, " ", end="")
+    print(x,"",end="")
